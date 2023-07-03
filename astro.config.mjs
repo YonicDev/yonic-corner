0f8deceb021f8d8fb21fc05cb92a31ed799d3239
Mario Martínez Palacio <yonicstudios@gmail.com>
2023-07-03 13:01:42 +0200

    Update Astro to 2.7.0 and dependencies
    (Astro 2.7.1 and above make breaking changes)

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,6 +38,6 @@ export default defineConfig({
     assets: true,
   },
   image: {
-    service: 'astro/assets/services/sharp',
+    service: { entrypoint: 'astro/assets/services/sharp' },
   }
 });
