df4490f5392761440679b889768350b5cd7aff15
Mario Martínez Palacio <yonicstudios@gmail.com>
2023-10-29 23:17:01 +0100

    Add new sprites and modularize site logo

--- src/components/MovingLogo.svelte
+++ src/components/MovingLogo.svelte
@@ -1,33 +1,36 @@
-<script lang="ts">
+<script>
     export let websiteText = "The Yonic Corner";
+	export let theme = "default";
 </script>
 
-<h1>Welcome to &nbsp;
-    <div class="bungee-container">
-        {#each websiteText.split("") as char,i}
-            {#if char===" "}
-                <span class="bungee" translate="no">&nbsp;</span>
-            {:else}
-                <span class="bungee" translate="no">{char}</span>
-            {/if}
-        {/each}
-        <div class="bungee-shadow">
-            {#each websiteText.split("") as char,i}
-                {#if char===" "}
-                    <span class="bungee" translate="no">&nbsp;</span>
-                {:else}
-                    <span class="bungee" translate="no">{char}</span>
-                {/if}
-            {/each}
-        </div>
-    </div>
-</h1>
+<div class="bungee-{theme}">
+	<div class="bungee-container">
+		{#each websiteText.split("") as char}
+			{#if char===" "}
+				<span class="bungee" translate="no">&nbsp;</span>
+			{:else}
+				<span class="bungee" translate="no">{char}</span>
+			{/if}
+		{/each}
+		<div class="bungee-shadow">
+			{#each websiteText.split("") as char}
+				{#if char===" "}
+					<span class="bungee" translate="no">&nbsp;</span>
+				{:else}
+					<span class="bungee" translate="no">{char}</span>
+				{/if}
+			{/each}
+		</div>
+	</div>
+</div>
 
 <style lang="scss">
     @use "../styles/util.scss";
     @use "../styles/vars.scss" as *;
 
-    @for $i from 1 through 16 {
+	$bungee-length: 16;
+
+    @for $i from 1 through $bungee-length {
         .bungee-container > .bungee:nth-child(#{$i}), .bungee-container > .bungee-shadow > span:nth-child(#{$i}) {
             animation-delay: #{$i * 0.1}s;
         }
@@ -62,6 +65,24 @@
 		}
 	}
 
+	.bungee-legacy > .bungee-container {
+		.bungee {
+			animation-name: logo-legacy;
+			animation-duration: 2s;
+			animation-timing-function: steps(#{$bungee-length}, start)!important;
+			text-shadow: #{util.extrude(6, #00322e)};
+			-webkit-text-stroke: 2.5px #002a26;
+		}
+		.bungee-shadow {
+			display: none;
+		}
+		@for $i from 1 through 16 {
+			> .bungee:nth-child(#{$i}) {
+				animation-delay: -#{$i * 0.1}s;
+			}
+		}
+	}
+
     @keyframes logo {
 		0%, 100% {
 			color: #0b396d;
@@ -84,6 +105,24 @@
 		50% {
 			opacity: 0.5;
 			filter: blur(3px);
+		}
+	}
+
+	@keyframes logo-legacy {
+		0%,100% {
+			color: #0041d0;
+			transform: translate3d(0px, 0px, 0px);
+		}
+		50% {
+			color: #00fcff;
+		}
+		85% {
+			color: #00f14c;
+			transform: translate3d(0px, 0px, 0px);
+		}
+		90% {
+			color: #0041d0;
+			transform: translate3d(0, -15px, 0px);
 		}
 	}
 </style>
