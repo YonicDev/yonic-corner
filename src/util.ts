cfdf238648ed260b54663dd3ef0dfd43f8c60496
Mario Martínez Palacio <yonicstudios@gmail.com>
2024-02-18 11:43:26 +0100

    Use new post hero default location

--- src/util.ts
+++ src/util.ts
@@ -9,7 +9,10 @@ export async function getHeroImages(posts: CollectionEntry<"blog">[]) {
         // But the file *has* to be an article asset named hero.png,
         // regardless of what the image type actually is.
         try {
-            const imageMeta: ImageMetadata = post.data.hero?.modern ?? (await import(`./assets/articles/${post.slug}/hero.png`)).default;
+            const [year, month, id] = post.slug.split("/");
+            const imageMeta: ImageMetadata = post.data.hero?.modern ?? (year === "drafts" 
+                ? (await import(`./content/blog/${year}/${month}/hero.png`)).default
+                : (await import(`./content/blog/${year}/${month}/${id}/hero.png`)).default);
             const processedImage = await getImage({ src: imageMeta, width: 550, height: 280, format: "webp"});
             return Promise.resolve(processedImage.src);
         } catch (err: any) {
