e8f6bd73416676314cd51d04def73df1ca338d3e
Mario Martínez Palacio <yonicstudios@gmail.com>
2024-02-18 12:25:43 +0100

    Update CLI to new content format

--- cli.mjs
+++ cli.mjs
@@ -4,15 +4,20 @@ import path from "path";
 import matter from "gray-matter";
 
 import { program } from "commander";
+import { globSync } from "glob";
 
 const updatePost = (updateFunction) => {
-    return (id, options) => {
-        const post = fs.readFileSync(path.resolve(process.cwd(), "./src/content/blog/", `${id}.mdx`))
+    return (id, date, options) => {
+        const [postPath] = globSync(`./src/content/blog/${date ?? "*/*"}/${id}/index.mdx`);
+        if(!postPath)
+            throw `There is no post with ID ${id}.`
+
+        const post = fs.readFileSync(postPath, 'utf-8')
         const { data, content } = matter(post);
     
         const output = updateFunction(data, content, options);
     
-        fs.writeFileSync(path.resolve(process.cwd(), "./src/content/blog/", `${id}.mdx`), output);
+        fs.writeFileSync(postPath, output);
         console.log("Post updated successfully!");
     }
 }
@@ -22,24 +27,35 @@ program
     .description("Creates a new post and its associated assets folder.")
     .argument('<title>', 'title of the post')
     .option('--id <id>', 'internal ID of the post')
-    .action((title, options) => {
+    .action(async (title, options) => {
         const id = options.id
-            ? options.id.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, "").replace(/\s/, "-").toLowerCase()
-            : title.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, "").replace(/\s/, "-").toLowerCase();
+            ? options.id.replaceAll(/[&\/\\#,+()$~%.'":*?<>{}]/g, "").replaceAll(/\s/g, "-").toLowerCase()
+            : title.replaceAll(/[&\/\\#,+()$~%.'":*?<>{}]/g, "").replaceAll(/\s/g, "-").toLowerCase();
 
-        const postDir = path.resolve(process.cwd(), "./src/content/blog/", `${id}.mdx`);
-        const assetsDir = path.resolve(process.cwd(), "./src/assets/articles", id);
+        const today = new Date();
+        const year = today.getUTCFullYear().toString();
+        const month = (today.getUTCMonth()+1).toString().padStart(2, '0');
 
-        if(fs.existsSync(postDir))
+        const postDir = path.resolve(process.cwd(), "./src/content/blog/", year, month, id);
+        const postPath = path.resolve(postDir, "index.mdx");
+
+        if(!fs.existsSync(postDir)) {
+            fs.mkdirSync(postDir, {recursive: true})
+        } else if(fs.existsSync(postPath)) {
             throw `A post with the ID ${id} already exists. Try with another ID.`
+        }
 
         const frontmatter = {
             title,
             description: "Insert description here",
-            pubDate: new Date().toISOString(),
+            pubDate: today.toISOString(),
+            hero: {
+                modern: "./hero.png",
+                legacy: "./hero-legacy.png"
+            },
             draft: true,
             category: "misc",
-            tags: []
+            tags: ['tags','here']
         }
         const content =`
 Image imports go here...
@@ -54,20 +70,21 @@ Body of the post here...`;
         const output = matter.stringify(content, frontmatter);
 
         
-        fs.writeFileSync(postDir, output);
+        fs.writeFileSync(postPath, output);
         console.log("Created MDX post!");
-        if(fs.existsSync(assetsDir) && fs.statSync(assetsDir).isDirectory()) {
-            console.log("Assets folder for this post already exists.")
-        } else {
-            fs.mkdirSync(assetsDir);
-            console.log("Created post assets folder!");
-        }
+
+        const placeholderModern = Buffer.from(await (await fetch("https://placehold.co/1920x1080/png")).arrayBuffer());
+        const placeholderLegacy = Buffer.from(await (await fetch("https://placehold.co/454x303/png")).arrayBuffer());
+
+        fs.writeFileSync(path.resolve(postDir, "hero.png"), placeholderModern);
+        fs.writeFileSync(path.resolve(postDir, "hero-legacy.png"), placeholderLegacy);
     });
 
 program
     .command("publish")
     .description("Sets post for publishing. This also updates the publish date.")
     .argument("<id>", "ID of the post")
+    .argument("[date-folder]", "The date folder it is included, in YYYY/MM format. If not specified, it gets the first find.")
     .option("-d", "do not update the publish date.")
     .action(updatePost((data, content, options) => {
         if(!options?.d)
@@ -80,6 +97,7 @@ program
     .command("update")
     .description("Marks posts that has been updated. The updated date will be set.")
     .argument("<id>", "ID of the post")
+    .argument("[date-folder]", "The date folder it is included, in YYYY/MM format. If not specified, it gets the first find.")
     .option("-p --publish", "Alter the publish date instead.")
     .action(updatePost((data, content, options) => {
         if(options?.publish) {
