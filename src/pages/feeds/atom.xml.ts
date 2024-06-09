ace63b8d4d3eedcb06abd5ae9b58a00a51483e64
Mario Martínez Palacio <yonicstudios@gmail.com>
2024-06-09 21:42:27 +0200

    Update to Astro 4.10.1

--- src/pages/feeds/atom.xml.ts
+++ src/pages/feeds/atom.xml.ts
@@ -1,20 +1,16 @@
 import AtomFeed from "@lib/components/feed/AtomFeed.astro";
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { getContainerRenderer as MDXContainerRenderer } from "@astrojs/mdx"
+import { getContainerRenderer as SvelteContainerRenderer } from "@astrojs/svelte"
+import { loadRenderers } from "astro/virtual-modules/container.js"; // Why no "astro:container"?
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({request, locals}) => {
-    const container = await AstroContainer.create({
-        renderers: [
-            {
-                name: "@astrojs/mdx",
-                serverEntrypoint: "astro/jsx/server.js",
-            },
-            {
-                name: "@astrojs/svelte",
-                serverEntrypoint: "@astrojs/svelte/server.js",
-            }
-        ],
-    });
+    const renderers = await loadRenderers([
+        MDXContainerRenderer(),
+        SvelteContainerRenderer()
+    ])
+    const container = await AstroContainer.create({ renderers });
 
     const xmlString = await container.renderToString(AtomFeed, { request, locals });
     return new Response(xmlString.replace("<!DOCTYPE html>", ""), {
